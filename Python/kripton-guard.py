9c8851961fee4d9613863f52139720113f15cbe3
Okan Özdemir <okanozdemir@stu.comu.edu.tr>
2018-03-02 19:38:20 +0300

    Fixes a bug that causes the new MAC addresses not to be inserted to database.

--- Python/kripton-guard.py
+++ Python/kripton-guard.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-#LAN Scanner
-
 from scapy.all import *
 from pyfcm import FCMNotification
 import configparser
@@ -49,7 +47,7 @@ def createTables(conn):
 
 def showDevices():
     #Shows devices in whitelist
-    print "\n========== Your Devices ==========\n    Mac Address       IP Address   "
+    print "\n========== Your Whitelist ==========\n    Mac Address       IP Address   "
     query = "SELECT macAddress,ipAddress FROM mac_ip_addresses;"
     result = conn.execute(query)
     for row in result:
@@ -66,7 +64,7 @@ ans,unans=srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=subnet), timeout=5, iface=
 
 if(config['SETTINGS']['firstTime']=='1'):
     config['SETTINGS']['firstTime']='0'
-    with open('kripton-guard.conf','w') as configfile:
+    with open('/etc/kripton-guard/kripton-guard.conf','w') as configfile:
         config.write(configfile)
     createTables(conn)
     for s,r in ans:
@@ -86,19 +84,25 @@ else:
         row = result.fetchone()
         if(row):
             if(row[1] != ip):
-                reply = raw_input(mac + " this address is already in whitelist with {0} IP address.\nWould you like to update IP adress to {1} y/n :".format(row[1],ip))
-                if (reply == 'e'):
-                    query = "UPDATE mac_ip_addresses SET ipAddress = '{0}' WHERE macAddress = '{1}';".format(ip,mac)
-                    conn.execute(query)
-                    conn.commit()
-                    print "Updated: " + mac + " -- " + ip
-        else:
-            sendNotification()
-            reply = raw_input("A new device has been detected.\nMac Address: {0} IP Address: {1}\nWould you like to add this device to whitelist? y/n :".format(mac,ip))
-            if (reply == 'e'):
-                query = "INSERT INTO mac_ip_addresses (macAddress, ipAddress) VALUES ('{0}','{1}');".format(mac, ip)
+                query = "UPDATE mac_ip_addresses SET ipAddress = '{0}' WHERE macAddress = '{1}';".format(ip,mac)
                 conn.execute(query)
                 conn.commit()
+#                reply = raw_input(mac + " this address is already in whitelist with {0} IP address.\nWould you like to update IP adress to {1} y/n :".format(row[1],ip))
+#                if (reply == 'y'):
+#                    query = "UPDATE mac_ip_addresses SET ipAddress = '{0}' WHERE macAddress = '{1}';".format(ip,mac)
+#                    conn.execute(query)
+#                    conn.commit()
+#                    print "Updated: " + mac + " -- " + ip""
+        else:
+            sendNotification()
+            query = "INSERT INTO mac_ip_addresses (macAddress, ipAddress) VALUES ('{0}','{1}');".format(mac, ip)
+            conn.execute(query)
+            conn.commit()
+#            reply = raw_input("A new device has been detected.\nMac Address: {0} IP Address: {1}\nWould you like to add this device to whitelist? y/n :".format(mac,ip))
+#            if (reply == 'y'):
+#                query = "INSERT INTO mac_ip_addresses (macAddress, ipAddress) VALUES ('{0}','{1}');".format(mac, ip)
+#                conn.execute(query)
+#                conn.commit()
 
 
 conn.close()
